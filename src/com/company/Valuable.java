ea7b8b2fc7fd253b642f5a995f2d07db32f6489f
elennatchk <80887068+elennatchk@users.noreply.github.com>
2021-10-25 16:46:15 +0400

    Update Valuable.java

--- src/com/company/Valuable.java
+++ src/com/company/Valuable.java
@@ -8,34 +8,3 @@ public interface Valuable extends Comparable<Valuable> {
 
     public double getValue() ;
 }
-//if (amount > Balance()) {
-//        System.out.println("null");
-//        return null;
-//        }
-//        if (amount <= 0) {
-//        System.out.println("null");
-//        return null;
-//        }
-//        Coin[] newarr= new Coin[]{};
-//        if (Balance() >= amount){
-//        for (int i=coins.length -1 ; i>=0;i--){
-//        if (amount - coins[i].getValue()>=0){
-//        amount-=coins[i].getValue();
-//        coins = Arrays.copyOf(coins, coins.length - 1);
-////                    System.out.println("what");
-//        System.out.println(coins.length);
-//
-//
-//
-//        }
-//        }
-//        }for (int i=0, j=0; i < newarr.length; i++){
-//        newarr[j++]=coins[i];
-//        System.out.println("is");
-////            System.out.println(coins.length);
-//
-//
-//        }
-//        Coin[] coin= new Coin[newarr.length];
-//
-//        return coin;
